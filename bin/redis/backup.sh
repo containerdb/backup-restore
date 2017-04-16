e4233858d1be2cb89cbd837c28267f9906b0387b
Scott Robertson <scottymeuk@gmail.com>
2017-04-16 15:43:46 +0100

    We need redis installed...

--- bin/redis/backup.sh
+++ bin/redis/backup.sh
@@ -1,3 +1,3 @@
 FILE_PATH="/tmp/$FILE_NAME"
-redis-cli -h $REDIS_HOST -a $REDIS_PASSWORD -p $REDIS_PORT --rdb - SAVE > $FILE_NAME
+redis-cli -h $REDIS_HOST -a $REDIS_PASSWORD -p $REDIS_PORT --rdb - SAVE > $FILE_NAME \ &&
 /usr/bin/s3cmd --access_key=$AWS_ACCESS_TOKEN --secret_key=$AWS_SECRET_KEY put $FILE_PATH s3://$AWS_BUCKET_NAME/$FILE_NAME
