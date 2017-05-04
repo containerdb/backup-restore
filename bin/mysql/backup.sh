80c9d30266b661c1d64c1532de67b96d5958a4c0
Marc Qualie <marc@marcqualie.com>
2017-05-04 14:57:35 +0100

    Add --verbose flag for database backups

--- bin/mysql/backup.sh
+++ bin/mysql/backup.sh
@@ -1,3 +1,4 @@
 FILE_PATH="/tmp/$FILE_NAME"
-mysqldump --host=$DB_HOST --user=$DB_USER --port=$DB_PORT --password=$DB_PASS --all-databases > $FILE_PATH
-/usr/bin/s3cmd --access_key=$AWS_ACCESS_TOKEN --secret_key=$AWS_SECRET_KEY put $FILE_PATH s3://$AWS_BUCKET_NAME/$FILE_NAME
+echo "Exporting to ${FILE_PATH}"
+mysqldump --verbose --host=$DB_HOST --user=$DB_USER --port=$DB_PORT --password=$DB_PASS --all-databases > $FILE_PATH
+/usr/bin/s3cmd --verbose --access_key=$AWS_ACCESS_TOKEN --secret_key=$AWS_SECRET_KEY put $FILE_PATH s3://$AWS_BUCKET_NAME/$FILE_NAME
