2cd72343eb9104d467735a4fab0f4cf1aa118318
Scott Robertson <scottymeuk@gmail.com>
2017-06-03 19:24:28 +0100

    S3 backups working

--- bin/postgres/backup.sh
+++ bin/postgres/backup.sh
@@ -1,8 +1,8 @@
-printenv
-
-FILE_PATH="/tmp/$FILE_NAME"
-PGPASSWORD=$DB_PASS pg_dumpall -h $DB_HOST -U $DB_USER -p $DB_PORT > $FILE_PATH || exit 1
+export FILE_PATH="/tmp/$FILE_NAME"
+PGPASSWORD=$DB_PASS pg_dumpall -h $DB_HOST -U $DB_USER -p $DB_PORT > $FILE_PATH
 
 if [ $BACKUP_PROVIDER = "s3" ]; then
-  sh /app/s3/put.sh
+  sh /app/backup/s3.sh || exit 1
+elif [ $BACKUP_PROVIDER = "local" ]; then
+  sh /app/backup/local.sh || exit 1
 fi
