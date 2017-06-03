cf6053f1391554005d2e4c4bd745d68b4862f056
Scott Robertson <scottymeuk@gmail.com>
2017-06-03 17:59:25 +0100

    Fixing path

--- bin/postgres/backup.sh
+++ bin/postgres/backup.sh
@@ -2,5 +2,5 @@ FILE_PATH="/tmp/$FILE_NAME"
 PGPASSWORD=$DB_PASS pg_dumpall -h $DB_HOST -U $DB_USER -p $DB_PORT > $FILE_PATH || exit 1
 
 if [ $BACKUP_PROVIDER = "s3" ]; then
-  sh /app/bin/s3/put.sh
+  sh /app/s3/put.sh
 fi
