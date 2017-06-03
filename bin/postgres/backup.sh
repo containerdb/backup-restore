0600960c59a86fa9127b79a169764f3ffb457b77
Scott Robertson <scottymeuk@gmail.com>
2017-06-03 14:16:24 +0100

    Startlng to support more backup providers

--- bin/postgres/backup.sh
+++ bin/postgres/backup.sh
@@ -1,3 +1,6 @@
 FILE_PATH="/tmp/$FILE_NAME"
-PGPASSWORD=$DB_PASS pg_dumpall -h $DB_HOST -U $DB_USER -p $DB_PORT > $FILE_PATH && \
-/usr/bin/s3cmd --access_key=$AWS_ACCESS_TOKEN --secret_key=$AWS_SECRET_KEY put $FILE_PATH s3://$AWS_BUCKET_NAME/$FILE_NAME
+PGPASSWORD=$DB_PASS pg_dumpall -h $DB_HOST -U $DB_USER -p $DB_PORT > $FILE_PATH || exit 1
+
+if [ $BACKUP_PROVIDER = "s3" ]; then
+  sh /app/bin/s3/put.sh
+fi
---
+++ bin/s3/put.sh
@@ -0,0 +1,1 @@
+/usr/bin/s3cmd --access_key=$AWS_ACCESS_TOKEN --secret_key=$AWS_SECRET_KEY put $FILE_PATH s3://$AWS_BUCKET_NAME/$FILE_NAME
